7b4c7e0d9fe4405a970e01a7abbc674a766aed8d
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-07-16 23:30:58 -0300

    M06

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const path = require('path')
 const port = process.env.PORT || 3000
 
 const { convert, toMoney } = require('./lib/convert')
+const apiBCB = require('./lib/bcb.api')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', async (req, res) => {
+    const cotacao = await apiBCB.getCotacao()
+    res.render('home',  { 
+        cotacao
+    })
 })
 
 app.get('/cotacao', (req, res) => {
